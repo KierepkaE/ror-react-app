5792c93286879c5adc113ee4f04f5f9fbea936d6
kierepkae <kierepkaedyta@gmail.com>
2019-10-06 12:41:32 +0100

    user can display fruits on the root path

---
+++ app/javascript/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+import AllFruits from './AllFruits'
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Fruits are great!</h1>
+        <AllFruits />
+      </div>
+    );
+  }
+}
+
+export default Main
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_06_102152) do
+ActiveRecord::Schema.define(version: 2019_10_06_111540) do
 
   create_table "fruits", force: :cascade do |t|
     t.string "name"
